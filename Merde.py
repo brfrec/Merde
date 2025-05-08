a453a68ab44aef4aaf10c0f8d88bcd86159707ec
brfrec <charloutre34@gmail.com>
2025-05-08 21:20:14 +0200

    code python avancé

--- Merde.py
+++ Merde.py
@@ -1,1 +1,10 @@
 print("Va te faire enculer")
+print("Veux tu jouer avec moi ?")
+choixJouerAvecMoi = int(input"\n1 - OUI\n2 - NON")
+if choixJouerAvecMoi == 1:
+    print("Génial")
+elif choixJouerAvecMoi == 2:
+    print("T'es méchant")
+else:
+    print("J'ai pas demande ça")
+
